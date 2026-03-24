e7f9d791b0bfc15557439de6c570795db08cb1b3
TruePaInt <>
2026-03-24 20:03:06 +0500

    hm

---
+++ Blazor/Blazor/Components/Pages/Fibonachi.razor.cs
@@ -0,0 +1,29 @@
+﻿using System.Numerics;
+
+namespace Blazor.Components.Pages
+{
+    public partial class Fibonachi
+    {
+        int number;
+        BigInteger fibonachi = 1;
+        List<BigInteger> results = new List<BigInteger>();
+        void setNumber(int number)
+        {
+            this.number = number;
+        }
+        void Calculate()
+        {
+            results = new List<BigInteger>();
+            fibonachi = 0;
+            results.Add(fibonachi);
+            fibonachi = 1;
+            results.Add(fibonachi);
+            for (int i = 1; i < number; i++)
+            {
+                fibonachi = results[i-1]+fibonachi;
+                //fibonachi = results[i-1]+fibonachi;
+                results.Add(fibonachi);
+            }
+        }
+    }
+}
