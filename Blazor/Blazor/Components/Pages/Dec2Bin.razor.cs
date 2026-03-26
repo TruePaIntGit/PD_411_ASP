b0878642b24095d05be6975d05bd8610a2581560
TruePaInt <>
2026-03-26 19:57:33 +0500

    hm

---
+++ Blazor/Blazor/Components/Pages/Dec2Bin.razor.cs
@@ -0,0 +1,12 @@
+﻿namespace Blazor.Components.Pages
+{
+    public partial class Dec2Bin
+    {
+        int number;
+        string data="0";
+        void Calculate()
+        {
+            data = Convert.ToString(number,2);
+        }
+    }
+}
