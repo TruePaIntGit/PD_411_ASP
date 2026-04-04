5704dc32a26c50604843be4bfcdc85bcc91d9e18
TruePaInt <>
2026-04-04 18:24:38 +0500

    hm

---
+++ Blazor/Academy/Models/Direction.cs
@@ -0,0 +1,24 @@
+﻿using Academy.Models.ValidationAttributes;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace Academy.Models
+{
+	[Index(nameof(direction_name), IsUnique = true)]
+	public class Direction
+	{
+		[Key]
+		public byte direction_id { get; set; }
+		[Required]
+		[UniqueDirectionName(ErrorMessage = "Error: такое направление уже существует")]
+		public string direction_name { get; set; }
+		//public override bool Equals(object? other)
+		//{
+		//	return this.direction_name.Equals((other as Direction).direction_name);
+		//}
+		//public override int GetHashCode()
+		//{
+		//	return HashCode.Combine(direction_name);
+		//}
+	}
+}
